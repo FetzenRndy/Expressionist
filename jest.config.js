2986c19f06424ad5cd59dc77c8cbe3835e0c1eb3
Patrick Hollweck <patrick-hollweck@gmx.de>
2019-04-13 22:32:40 +0200

    Setup jest + tests

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	"roots": [
+		"<rootDir>/test/unit"
+	],
+	"transform": {
+		"^.+\\.tsx?$": "ts-jest"
+	},
+	"testRegex": "(test|spec).ts",
+	"moduleFileExtensions": [
+		"ts",
+		"tsx",
+		"js",
+		"jsx",
+		"json",
+		"node"
+	],
+};
---
+++ test/unit/expressionist.test.ts
@@ -0,0 +1,18 @@
+import { expressionist } from "../../src/expressionist";
+
+const defaultOptions = {
+	startChar: "{",
+	endChar: "}",
+};
+
+describe('Expressionist', () => {
+
+	it('Should parse simple expressions', () => {
+		expect(expressionist("{1+1}", defaultOptions)).toEqual("2");
+	});
+
+	it("should parse nested expressions", () => {
+		expect(expressionist("{ 1 + { 1 + 1 } }", defaultOptions)).toEqual("3");
+	});
+
+});
