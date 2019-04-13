d65b40a53292fa4341b1c85fa6e0cefe8564b301
Patrick Hollweck <patrick-hollweck@gmx.de>
2019-04-13 14:40:15 +0200

    Fix `isTextBox` implementation

--- src/DOMutil.ts
+++ src/DOMutil.ts
@@ -1,7 +1,6 @@
 export function isTextBox(element: Element): boolean {
 	return (
 		element instanceof HTMLInputElement &&
-		element.isContentEditable &&
 		typeof (element as HTMLInputElement).value !== "undefined"
 	);
 }
